d1d8b745ee04400a37aa00f86ad1838c895fd892
Guilherme-Luis <guilherme_silva_luis@hotmail.com>
2026-09-09 22:37:25 -0300

    Remove do repositorio os arquivos de uso local
    As pastas docs, examples, samples e scripts passam a ser ignoradas: os
    arquivos continuam localmente, mas fora do controle de versao.
    
    O consumidor deixa de expor a funcao usada apenas pela carga local de
    exemplos, mantendo somente o caminho da mensagem vinda do Pub/Sub.

--- src/services/ingestOrder.js
+++ src/services/ingestOrder.js
@@ -1,14 +1,9 @@
-const { normalizarPedido, parseMensagem } = require('../domain/orderPayload');
+const { parseMensagem } = require('../domain/orderPayload');
 const { salvarPedido } = require('../repositories/orderWriteRepository');
-
-async function ingerirPayload(raw, contexto = {}) {
-  const pedido = normalizarPedido(raw);
-  return salvarPedido(pedido, contexto);
-}
 
 async function ingerirMensagem(buffer, contexto = {}) {
   const pedido = parseMensagem(buffer);
   return salvarPedido(pedido, contexto);
 }
 
-module.exports = { ingerirPayload, ingerirMensagem };
+module.exports = { ingerirMensagem };
